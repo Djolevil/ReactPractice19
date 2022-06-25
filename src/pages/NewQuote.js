3fcb7d3d5052de999d2740ce9e12a70d54220445
Djordje Knezevic <starwarrior@mycity.rs>
2022-06-25 16:53:49 +0200

    Use programmatic navigation to go to AllQuotes page after submitting new quote

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,9 +1,14 @@
+import { useHistory } from 'react-router-dom';
+
 import QuoteForm from '../components/quotes/QuoteForm';
 
 const NewQuote = () => {
+    const history = useHistory();
     const addQuoteHandler = (quoteData) => {
         //will send data to server later
         console.log(quoteData);
+
+        history.push('/quotes');
     };
     return <QuoteForm onAddQuote={addQuoteHandler}/>
 };
