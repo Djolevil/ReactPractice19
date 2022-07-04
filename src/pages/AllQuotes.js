4ace574c86bde28e1b89a1f73c7dcccb09775e86
Djordje Knezevic <starwarrior@mycity.rs>
2022-07-04 20:41:02 +0200

    Add logic for sending and getting quote data via http

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,13 +1,40 @@
+import { useEffect } from 'react';
 import QuoteList from '../components/quotes/QuoteList';
+import LoadingSpinner from '../components/UI/LoadingSpinner';
+import NoQuotesFound from '../components/quotes/NoQuotesFound';
 
-const DUMMY_QUOTES = [
-    { id: 'q1', author: 'Quark', text: 'A contract is a contract is a contract!' },
-    { id: 'q2', author: 'Picard', text: 'The line must be drawn here, this far, no further!' },
-];
+import useHttp from '../hooks/use-http';
+import { getAllQuotes } from '../lib/api';
 
 const AllQuotes = () => {
-    return <QuoteList quotes={DUMMY_QUOTES} />
-           
+  const {
+    sendRequest,
+    status,
+    data: loadedQuotes,
+    error,
+  } = useHttp(getAllQuotes, true);
+
+  useEffect(() => {
+    sendRequest();
+  }, [sendRequest]);
+
+  if (status === 'pending') {
+    return (
+      <div className='centered'>
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <p className='centered focused'>{error}</p>;
+  }
+
+  if (status === 'completed' && (!loadedQuotes || loadedQuotes.length === 0)) {
+    return <NoQuotesFound />;
+  }
+
+  return <QuoteList quotes={loadedQuotes} />;
 };
 
 export default AllQuotes;
